09384a9a1923869bfa58379a56224f403fb3d631
ASEM msaduh <em17@smail.ucas.edu.ps>
2026-08-20 12:18:31 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,20 +2,18 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::updateOrCreate(
-            ['email' => 'admin@gmail.com'],
-            [
-                'name' => 'Admin',
-                'password' => Hash::make('12345678'), // كلمة السر هنا
-            ]
-        );
+        User::create([
+            'name' => 'Mohammad',
+            'email' => 'mohammad@gmail.com', // أو البريد الذي تفضله
+            'password' => Hash::make('123456789123456789'),
+        ]);
     }
 }
